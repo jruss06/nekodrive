4a77b4b6548d3cec5d9e2ae237568501f9e84bae
James Russo <james.russo@sphereco.com>
2022-04-13 12:46:30 -0400

    add example scanning

---
+++ NFSClientExample/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NFSClientExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var scanner = new Scan();
+            scanner.Run();
+        }
+    }
+}
---
+++ NFSClientExample/Scan.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using NFSLibrary;
+using org.acplt.oncrpc;
+
+namespace NFSClientExample
+{
+    public class Scan
+    {
+        public void Run()
+        {
+            var client = new NFSClient(NFSClient.NFSVersion.v3);
+
+            client.Connect(IPAddress.Parse("127.0.0.1"), 0, 0, 30000, Encoding.ASCII, true, OncRpcProtocols.ONCRPC_TCP);
+
+            var exports = client.GetExportedDevices();
+
+            foreach (var export in exports) 
+            {
+                Console.WriteLine(export);
+
+                client.MountDevice(export);
+
+                var children = client.GetItemList(".");
+
+                foreach (var child in children)
+                {
+                    Console.WriteLine(child);
+                }
+            }
+
+        }
+    }
+}
